114d278acb4226a745958d6d5b78614fec681087
Bharat Goyal <bharatgoyal@Bharats-MacBook-Air.local>
2020-07-26 23:11:10 +0530

    added docker

--- django_backend/djangoenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/bharatgoyal/Documents/django-projects/django-ecommerce/django_react_boilerplate/django_backend/djangoenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
